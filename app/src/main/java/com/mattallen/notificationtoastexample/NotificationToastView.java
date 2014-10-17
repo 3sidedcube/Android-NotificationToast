877eeec4af013b993f7e8406f3ebcfa5ac9cc4ba
Matt Allen <mattallen092@gmail.com>
2014-10-17 14:56:46 +0100

    Now animating the view and showing the next in the queue

--- app/src/main/java/com/mattallen/notificationtoastexample/NotificationToastView.java
+++ app/src/main/java/com/mattallen/notificationtoastexample/NotificationToastView.java
@@ -1,8 +1,12 @@
 package com.mattallen.notificationtoastexample;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.widget.LinearLayout;
 
 /**
  * A small view to be displayed at the top of the window and will pop down and swipe away after a
@@ -11,17 +15,62 @@ import android.view.View;
  * @author Matt Allen
  * @project Blood
  */
-public class NotificationToastView extends View
+public class NotificationToastView extends LinearLayout
 {
 	private OnNotificationToastEnd mCallback;
+	private AnimatorListener mListener;
+	private long mDuration;
 
-	public NotificationToastView(Context context, AttributeSet attrs, int defStyleAttr)
+	public NotificationToastView(Context context, AttributeSet attrs)
 	{
-		super(context, attrs, defStyleAttr);
+		super(context, attrs);
 	}
 
 	public void setCallback(OnNotificationToastEnd callback)
 	{
 		mCallback = callback;
+
+		mListener = new AnimatorListener()
+		{
+			@Override public void onAnimationStart(Animator animation)
+			{
+
+			}
+
+			@Override public void onAnimationEnd(Animator animation)
+			{
+				mCallback.onNotificationToastEnd();
+			}
+
+			@Override public void onAnimationCancel(Animator animation)
+			{
+				mCallback.onNotificationToastEnd();
+			}
+
+			@Override public void onAnimationRepeat(Animator animation)
+			{
+
+			}
+		};
+	}
+
+	public void setDuration(long duration)
+	{
+		mDuration = duration;
+	}
+
+	@Override
+	public void onDraw(Canvas canvas)
+	{
+		super.onDraw(canvas);
+
+		// Set the animation for when the view has finished
+		ViewPropertyAnimator animator = animate();
+		animator.setStartDelay(mDuration);
+		animator.setListener(mListener);
+		animator.setDuration(500);
+		animator.alpha(0f);
+		animator.x(2f);
+		animator.start();
 	}
 }
